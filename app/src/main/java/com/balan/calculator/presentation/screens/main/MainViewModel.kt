2f21b7734a1db3d452220a64c4aa31736dc0fd1b
boonapart <kolyha101@gmail.com>
2024-03-01 18:37:00 +0300

    refactor code domain

--- app/src/main/java/com/balan/calculator/presentation/screens/main/MainViewModel.kt
+++ app/src/main/java/com/balan/calculator/presentation/screens/main/MainViewModel.kt
@@ -1,11 +1,11 @@
-package com.balan.calculator.domain.viewmodel
+package com.balan.calculator.presentation.screens.main
 
 import androidx.lifecycle.ViewModel
 import com.balan.calculator.domain.model.NumericSymbolButton
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class MainScreenViewModel : ViewModel() {
+class MainViewModel : ViewModel() {
    private val _state : MutableStateFlow<NumericSymbolButton> = MutableStateFlow(NumericSymbolButton())
     val state = _state.asStateFlow()
 }
